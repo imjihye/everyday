90d01806e307ba6f5c9bf4fd8e30a3b4d25daa5a
jihye <im@jihye.me>
2016-10-05 13:50:54 +0900

    add sense app

--- everyday/urls.py
+++ everyday/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^everyday/', include('diary.urls')),
+    url(r'^diary/', include('diary.urls')),
+    url(r'^sense/', include('sense.urls')),
 ]
